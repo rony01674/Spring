5425fe14204a29b855a326c37f869dc364a51ca4
rony01674 <rony01674>
2019-07-09 18:53:55 +0600

    spring security

---
+++ spring-security/src/main/java/com/springsecurity/repository/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.springsecurity.repository;
+
+import com.springsecurity.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
+}
