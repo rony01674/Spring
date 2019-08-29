400a0e7645613baf95afcb451bb9abbe865e56f2
rony01674 <rony01674@gmail.com>
2019-08-29 10:28:50 +0600

    project

--- demo-project/src/main/java/com/demoproject/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.demoproject.repository;
-
-import com.demoproject.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-}
